e6fcf67c2260a1a39ab362ef42b5dadf7e309329
firmboy <yangjian5268@gmail.com>
2020-09-30 10:26:26 +0800

    初始化提交

---
+++ src/main/java/com/playboy/keywords/KeyWordsPlugin.java
@@ -0,0 +1,121 @@
+package com.playboy.keywords;
+
+import com.google.common.collect.Sets;
+import com.playboy.util.ObjectUtils;
+import org.apache.ibatis.cache.CacheKey;
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Field;
+import java.util.*;
+
+/**
+ * @author playboy
+ * @create 2020-02-18 8:19 下午
+ * @description 过滤关键字的mybatis过滤器
+ * @serviceLogic
+ **/
+@Intercepts(
+        {
+                @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class}),
+                @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class, CacheKey.class, BoundSql.class}),
+        }
+)
+public class KeyWordsPlugin implements Interceptor {
+    private Logger log = LoggerFactory.getLogger(KeyWordsPlugin.class);
+
+    private boolean LOAD;//文件是否加载成功
+    private String KEY_WORDS = "";
+    private Set<String> KEYS = Sets.newHashSet();
+    private boolean ENABLE = false;
+    private Field additionalParametersField;
+
+    private static OracleSqlParser oracleSqlParser;
+
+
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        Object[] args = invocation.getArgs();
+        MappedStatement ms = (MappedStatement) args[0];
+        ms.getConfiguration();
+        Object parameter = args[1];
+        RowBounds rowBounds = (RowBounds) args[2];
+        ResultHandler resultHandler = (ResultHandler) args[3];
+        Executor executor = (Executor) invocation.getTarget();
+        CacheKey cacheKey;
+        BoundSql boundSql;
+        //由于逻辑关系，只会进入一次
+        if (args.length == 4) {
+            //4 个参数时
+            boundSql = ms.getBoundSql(parameter);
+            cacheKey = executor.createCacheKey(ms, parameter, rowBounds, boundSql);
+        } else {
+            //6 个参数时
+            cacheKey = (CacheKey) args[4];
+            boundSql = (BoundSql) args[5];
+        }
+        if (ENABLE) {
+            String sql = boundSql.getSql();
+
+            boolean containFlag = false;
+            for (String key : KEYS) {
+                if (sql.contains(key)) {
+                    containFlag = true;
+                    break;
+                }
+            }
+            if (containFlag) {
+                log.info("sql含关键字");
+                sql = oracleSqlParser.dealSql(sql);
+                Map<String, Object> additionalParameters = (Map<String, Object>) additionalParametersField.get(boundSql);
+                boundSql = new BoundSql(ms.getConfiguration(), sql, boundSql.getParameterMappings(), boundSql.getParameterObject());
+                for (String key : additionalParameters.keySet()) {
+                    boundSql.setAdditionalParameter(key, additionalParameters.get(key));
+                }
+            }
+        }
+        List resultList = executor.query(ms, parameter, rowBounds, resultHandler, cacheKey, boundSql);
+        return resultList;
+    }
+
+    @Override
+    public Object plugin(Object target) {
+        return Plugin.wrap(target, this);
+    }
+
+    @Override
+    public void setProperties(Properties prop) {
+        if (!ObjectUtils.isEmpty(prop.getProperty("keywords"))) {
+            KEY_WORDS = prop.getProperty("keywords");
+            log.info("设置的关键字：" + KEY_WORDS);
+            String[] split = KEY_WORDS.split(",");
+            List<String> strings = Arrays.asList(split);
+            KEYS = Sets.newHashSet(strings);
+            oracleSqlParser = new OracleSqlParser(KEY_WORDS);
+        }
+        if (!ObjectUtils.isEmpty(prop.getProperty("enable"))) {
+            String enable = prop.getProperty("enable");
+            if (enable.equals("1")) {
+                log.info("过滤关键字插件enable");
+                ENABLE = true;
+            }
+        }
+        try {
+            //反射获取 BoundSql 中的 additionalParameters 属性
+            additionalParametersField = BoundSql.class.getDeclaredField("additionalParameters");
+            additionalParametersField.setAccessible(true);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
+
---
+++ src/main/java/com/playboy/keywords/OracleSqlParser.java
@@ -0,0 +1,230 @@
+package com.playboy.keywords;
+
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.date.TimeInterval;
+import com.alibaba.druid.sql.SQLUtils;
+import com.alibaba.druid.sql.ast.SQLExpr;
+import com.alibaba.druid.sql.ast.SQLOver;
+import com.alibaba.druid.sql.ast.SQLStatement;
+import com.alibaba.druid.sql.ast.expr.*;
+import com.alibaba.druid.sql.ast.statement.*;
+import com.alibaba.druid.sql.dialect.oracle.ast.stmt.OracleSelectJoin;
+import com.alibaba.druid.sql.dialect.oracle.ast.stmt.OracleSelectSubqueryTableSource;
+import com.alibaba.druid.sql.dialect.oracle.ast.stmt.OracleSelectTableReference;
+import com.alibaba.druid.util.JdbcConstants;
+import com.playboy.util.ObjectUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+/**
+ * @author playboy
+ * @create 2020-02-21 8:23 下午
+ * @description oracle数据解析
+ * @serviceLogic
+ **/
+public class OracleSqlParser {
+    private Logger log = LoggerFactory.getLogger(OracleSqlParser.class);
+
+    private String keyWords;
+
+    public OracleSqlParser(String keyWords) {
+        this.keyWords = keyWords;
+    }
+
+
+    public String dealSql(String sql) {
+        TimeInterval timer = DateUtil.timer();
+
+        String oracle = JdbcConstants.ORACLE;
+        //1.解析
+        List<SQLStatement> sqlStatements = SQLUtils.parseStatements(sql, oracle);
+
+        //2.处理
+        sqlStatements.forEach(sqlStatement -> {
+            if (sqlStatement instanceof SQLSelectStatement) {
+                SQLSelectStatement selectStatement = (SQLSelectStatement) sqlStatement;
+                SQLSelect select = selectStatement.getSelect();
+                dealSelect(select);
+            } else if (sqlStatement instanceof SQLUpdateStatement) {
+                SQLUpdateStatement updateStatement = (SQLUpdateStatement) sqlStatement;
+                dealUpdate(updateStatement);
+            } else if (sqlStatement instanceof SQLInsertStatement) {
+                SQLInsertStatement insertStatement = (SQLInsertStatement) sqlStatement;
+                dealInsert(insertStatement);
+            }
+        });
+        //3.组装
+        String s = SQLUtils.toSQLString(sqlStatements, oracle);
+        return s;
+    }
+
+    /**
+     * 处理insert语句
+     *
+     * @param insert
+     */
+    public void dealInsert(SQLInsertStatement insert) {
+        if (ObjectUtils.isEmpty(insert)) {
+            return;
+        }
+        SQLExprTableSource tableSource = insert.getTableSource();
+        dealTableSource(tableSource);
+        List<SQLExpr> columns = insert.getColumns();
+        columns.forEach(column -> {
+            dealExpr(column);
+        });
+        List<SQLExpr> values = insert.getValues().getValues();
+        values.forEach(value -> {
+            dealExpr(value);
+        });
+        SQLSelect query = insert.getQuery();
+        dealSelect(query);
+    }
+
+    /**
+     * 处理update语句
+     *
+     * @param update
+     */
+    public void dealUpdate(SQLUpdateStatement update) {
+        if (ObjectUtils.isEmpty(update)) {
+            return;
+        }
+        SQLTableSource tableSource = update.getTableSource();
+        dealTableSource(tableSource);
+
+        List<SQLUpdateSetItem> items = update.getItems();
+        items.forEach(sqlUpdateSetItem -> {
+            SQLExpr column = sqlUpdateSetItem.getColumn();
+            dealExpr(column);
+            SQLExpr value = sqlUpdateSetItem.getValue();
+            dealExpr(value);
+        });
+
+        SQLExpr where = update.getWhere();
+        dealExpr(where);
+    }
+
+    /**
+     * 处理select语句
+     *
+     * @param select
+     */
+    public void dealSelect(SQLSelect select) {
+        if (ObjectUtils.isEmpty(select)) {
+            return;
+        }
+        SQLSelectQuery query = select.getQuery();
+        if (query instanceof SQLSelectQueryBlock) {
+            SQLSelectQueryBlock queryBlock = (SQLSelectQueryBlock) query;
+            List<SQLSelectItem> selectList = queryBlock.getSelectList();
+            selectList.forEach(sqlSelectItem -> {
+                String alias = sqlSelectItem.getAlias();
+                if (alias != null) {
+                    sqlSelectItem.setAlias(dealName(alias));
+                }
+                SQLExpr expr = sqlSelectItem.getExpr();
+                dealExpr(expr);
+            });
+            SQLTableSource from = queryBlock.getFrom();
+            dealTableSource(from);
+            SQLExpr where = queryBlock.getWhere();
+            dealExpr(where);
+        } else if (query instanceof SQLUnionQuery) {
+            SQLUnionQuery sqlUnionQuery = (SQLUnionQuery) query;
+            SQLSelectQuery left = sqlUnionQuery.getLeft();
+            SQLSelectQuery right = sqlUnionQuery.getRight();
+        }
+    }
+
+    /**
+     * @param tableSource 处理table表来源
+     */
+    public void dealTableSource(SQLTableSource tableSource) {
+        if (ObjectUtils.isEmpty(tableSource)) {
+            return;
+        }
+        if (tableSource instanceof OracleSelectJoin) {//联查
+            OracleSelectJoin oracleSelectJoin = (OracleSelectJoin) tableSource;
+            SQLTableSource left = oracleSelectJoin.getLeft();
+            dealTableSource(left);
+            SQLTableSource right = oracleSelectJoin.getRight();
+            dealTableSource(right);
+            SQLExpr condition = oracleSelectJoin.getCondition(); //TODO
+            dealExpr(condition);
+        } else if (tableSource instanceof OracleSelectSubqueryTableSource) { //子表
+            OracleSelectSubqueryTableSource oracleSelectSubqueryTableSource = (OracleSelectSubqueryTableSource) tableSource;
+            SQLSelect select = oracleSelectSubqueryTableSource.getSelect();
+            dealSelect(select);
+        } else if (tableSource instanceof OracleSelectTableReference) { //正式数据库表
+            OracleSelectTableReference oracleSelectTableReference = (OracleSelectTableReference) tableSource;
+        }
+    }
+
+    /**
+     * 处理各种表达式
+     *
+     * @param expr
+     */
+    public void dealExpr(SQLExpr expr) {
+        if (ObjectUtils.isEmpty(expr)) {
+            return;
+        }
+        if (expr instanceof SQLIdentifierExpr) {//BILL_ID
+            SQLIdentifierExpr sqlIdentifierExpr = (SQLIdentifierExpr) expr;
+            String name = sqlIdentifierExpr.getName();
+            sqlIdentifierExpr.setName(dealName(name));
+        } else if (expr instanceof SQLAggregateExpr) {//COUNT(DISTINCT b.BILL_ID)
+            SQLAggregateExpr sqlAggregateExpr = (SQLAggregateExpr) expr;
+            if (sqlAggregateExpr.getMethodName().equalsIgnoreCase("ROW_NUMBER")) {
+                SQLOver over = sqlAggregateExpr.getOver();
+                List<SQLExpr> partitionBys = over.getPartitionBy();
+                partitionBys.forEach(partitionBy -> {
+                    dealExpr(partitionBy);
+                });
+            } else {
+                List<SQLExpr> arguments = sqlAggregateExpr.getArguments();
+                arguments.forEach(argument -> {
+                    dealExpr(argument);
+                });
+            }
+        } else if (expr instanceof SQLPropertyExpr) {
+            SQLPropertyExpr sqlPropertyExpr = (SQLPropertyExpr) expr;
+            String name = sqlPropertyExpr.getName();
+            if (!name.equals("*")) {
+                sqlPropertyExpr.setName(dealName(name));
+            }
+        } else if (expr instanceof SQLMethodInvokeExpr) {
+            SQLMethodInvokeExpr sqlMethodInvokeExpr = (SQLMethodInvokeExpr) expr;
+            List<SQLExpr> parameters = sqlMethodInvokeExpr.getParameters();
+            parameters.forEach(parameter -> {
+                dealExpr(parameter);
+            });
+        } else if (expr instanceof SQLQueryExpr) {
+            SQLQueryExpr sqlQueryExpr = (SQLQueryExpr) expr;
+            SQLSelect subQuery = sqlQueryExpr.getSubQuery();
+            dealSelect(subQuery);
+        } else if (expr instanceof SQLBinaryOpExpr) {
+            SQLBinaryOpExpr sqlBinaryOpExpr = (SQLBinaryOpExpr) expr;
+            SQLExpr left = sqlBinaryOpExpr.getLeft();
+            dealExpr(left);
+            SQLExpr right = sqlBinaryOpExpr.getRight();
+            dealExpr(right);
+        } else if (expr instanceof SQLBetweenExpr) {
+            SQLBetweenExpr sqlBetweenExpr = (SQLBetweenExpr) expr;
+            SQLExpr testExpr = sqlBetweenExpr.getTestExpr();
+            dealExpr(testExpr);
+        }
+    }
+
+    public String dealName(String name) {
+        if (!ObjectUtils.isEmpty(name)) {
+            if (keyWords.contains(name)) {
+                return "\"" + name + "\"";
+            }
+        }
+        return name;
+    }
+}
